6b14e03d361702e8a8da686e9c15a53d52294dfc
Naresh Tak <taknaresh515@gmail.com>
2023-04-03 10:30:18 +0530

    fullscreen & closefullscreen

--- script.js
+++ script.js
@@ -233,3 +233,24 @@ new Chart("myChart", {
 
     }
 });
+
+var elem = document.documentElement;
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
